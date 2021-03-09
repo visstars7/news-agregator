4938513f075430d14b9e26929712b81275fffafd
hirasakavizu <havisikkubaru@gmail.com>
2021-03-09 15:06:42 +0700

    revisi shadow card

--- src/components/molecules/CardShadow/index.jsx
+++ src/components/molecules/CardShadow/index.jsx
@@ -6,7 +6,10 @@ const index = () => {
             <div className="flex flex-col">
                 <div className="w-40 mt-5 mr-5 h-3 bg-gray-300 rounded"></div>
                 <div className="w-28 mt-2 mr-5 h-3 bg-gray-300 rounded"></div>
-                <div className="w-20 mt-2 mr-5 h-3 bg-gray-300 rounded"></div>
+                <div className="flex flex-row">
+                    <div className="w-20 mt-2 mr-2 h-3 bg-gray-300 rounded"></div>
+                    <div className="w-16 mt-2 h-3 bg-gray-300 rounded"></div>
+                </div>
                 <div className="w-20 mt-5 mr-5 h-6 bg-gray-300 rounded"></div>
             </div>
             <div className="my-auto">
